b081a1c67dad688254b5a2f33b7ef179079afff0
Blootzz <106506598+Blootzz@users.noreply.github.com>
2024-07-15 21:19:13 -0500

    Finished Main menu, settings prefab

--- Assets/Code/UI/MainMenuInator.cs
+++ Assets/Code/UI/MainMenuInator.cs
@@ -5,14 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuInator : MonoBehaviour
 {
+    [SerializeField] GameObject optionsMenu;
+
     // called by play button
     public void _PlayGame()
     {
         SceneManager.LoadScene("Game");
     }
 
+    // called by settings button
     public void _OpenSettings()
     {
-        
+        optionsMenu.SetActive(true);
+        this.gameObject.SetActive(false);
     }
 }
