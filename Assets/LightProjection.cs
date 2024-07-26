b2d5563f194eaa761e9ff02fd94459ae3ded8ed9
minhalvakil <minhal@vakil.ca>
2024-07-25 23:27:43 -0400

    Added Mirror System to the LightProjection

--- Assets/LightProjection.cs
+++ Assets/LightProjection.cs
@@ -10,12 +10,22 @@ public class LightProjection : MonoBehaviour
     float maxDistance = 30f;
     [SerializeField]
     LayerMask layerMask;
+    [SerializeField]
+    Color color;
+    [SerializeField]
+    string ability;
+
     // Start is called before the first frame update
     void Start()
     {
-        Project();
+       onSetActive();
     }
 
+    void onSetActive()
+    {
+         this.gameObject.GetComponent<MeshRenderer>().material.color = color;
+        Project();
+    }
     // Update is called once per frame
     void Update()
     {
@@ -28,6 +38,7 @@ public class LightProjection : MonoBehaviour
         float scaleDist;
         if (Physics.Raycast(transform.parent.position, transform.parent.forward, out hit, maxDistance, layerMask))
         {
+            UpdateMirror(hit.collider);
             scaleDist = hit.distance/2;
             Debug.DrawLine(transform.parent.position, transform.parent.position + transform.parent.forward * hit.distance, Color.yellow, 10);
         }
@@ -37,25 +48,49 @@ public class LightProjection : MonoBehaviour
             Debug.DrawLine(transform.parent.position,transform.parent.position +  transform.parent.forward * 1000, Color.white, 10);
             Debug.Log("Did not Hit");
         }
-        
-        this.transform.localScale = new Vector3(this.transform.localScale.x, scaleDist, this.transform.localScale.z);
-        this.transform.localPosition = new Vector3(this.transform.localPosition.x, this.transform.localPosition.y, scaleDist);
+        ExtendObject(scaleDist);
     }
     void OnTriggerEnter(Collider collider)
     {
+        if(collider.tag == "LightProjection")
+            return;
+        UpdateMirror(collider); 
         float scaleDist = Vector3.Distance(transform.parent.position, collider.gameObject.transform.position)/2;
-        this.transform.localScale = new Vector3(this.transform.localScale.x, scaleDist, this.transform.localScale.z);
-        this.transform.localPosition = new Vector3(this.transform.localPosition.x, this.transform.localPosition.y, scaleDist);
+        ExtendObject(scaleDist);
     }
     void OnTriggerStay(Collider collider)
     {
+        if(collider.tag == "LightProjection")
+            return;
+        UpdateMirror(collider);
         float scaleDist = Vector3.Distance(transform.parent.position, collider.gameObject.transform.position)/2;
-        this.transform.localScale = new Vector3(this.transform.localScale.x, scaleDist, this.transform.localScale.z);
-        this.transform.localPosition = new Vector3(this.transform.localPosition.x, this.transform.localPosition.y, scaleDist);
+        ExtendObject(scaleDist);
     }
     void OnTriggerExit(Collider collider)
     {
+        if(collider.tag == "LightProjection")
+            return;
         Project();
+        if(collider.tag == "Mirror")
+        {
+            collider.transform.GetChild(0).gameObject.SetActive(false);
+        }
+    }
+    void ExtendObject(float scaleDist)
+    {
+        this.transform.localScale = new Vector3(this.transform.localScale.x, scaleDist+0.1f, this.transform.localScale.z);
+        this.transform.localPosition = new Vector3(this.transform.localPosition.x, this.transform.localPosition.y, scaleDist);
+
+    }
+    void UpdateMirror(Collider collider)
+    {
+        if(collider.tag == "Mirror")
+        {
+            collider.transform.GetChild(0).gameObject.SetActive(true);
+            collider.transform.GetChild(0).gameObject.GetComponent<LightProjection>().color = this.color;
+            collider.transform.GetChild(0).gameObject.GetComponent<LightProjection>().ability = this.ability;
+            collider.transform.GetChild(0).gameObject.GetComponent<LightProjection>().onSetActive();
+        }
     }
     
 }
